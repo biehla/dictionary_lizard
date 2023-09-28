59981e75e62a4e76e93c2ae6a7654ec9d14d259f
Anand Biehl <36610944+biehla@users.noreply.github.com>
2023-09-27 22:51:53 -0400

    got the freaking history to work!!!!!!!

--- src/game.zig
+++ src/game.zig
@@ -8,9 +8,11 @@ pub const Game = struct {
     label: *const [6]u8 = "Word: ",
     indicator: *const [1]u8 = "_",
     allocator: std.mem.Allocator,
-    historyList: [5]string,
-    name: [9:0]u8 = undefined,
-    letterCount: usize = 0,
+    historyList: [5][11]u8 = undefined,
+    historyListWordLen: [5]u8 = undefined,
+    historyListLen: u8 = 0,
+    name: [10]u8 = undefined,
+    letterCount: u8 = 0,
 
     pub fn tick(self: *Game) void {
         setVars(self) catch {
@@ -23,8 +25,6 @@ pub const Game = struct {
     }
 
     fn setVars(self: *Game) !void {
-        while (self.fullLabel.popOrNull() != null) continue;
-
         self.counter += 1;
 
         if (self.counter % 100 < 50) {
@@ -36,21 +36,31 @@ pub const Game = struct {
         raylib.setMouseCursor(@intFromEnum(raylib.MouseCursor.mouse_cursor_ibeam));
         var key: u32 = @intCast(raylib.getCharPressed());
         while (key > 0) {
-            if ((key >= 32) and (key <= 125) and (self.letterCount < self.name.len)) {
+            if ((key >= 32) and (key <= 125) and (self.letterCount < self.name.len) and self.letterCount < 9) {
                 self.name[self.letterCount] = @intCast(key);
-                self.name[self.letterCount+1] = 0;
+                self.name[self.letterCount + 1] = 0;
                 self.letterCount += 1;
             }
             key = @intCast(raylib.getCharPressed());
         }
-        
+
         if (raylib.isKeyPressed(.key_backspace) and self.letterCount != 0) {
+            self.name[self.letterCount] = 0;
             self.letterCount -= 1;
             self.name[self.letterCount] = 0;
-            
+
             if (self.letterCount <= 0) {
                 self.letterCount = 0;
             }
+        } else if (raylib.isKeyPressed(.key_enter) and self.letterCount >= 3) {
+            for (self.name, 0..) |_, idx| {
+                self.historyList[self.historyListLen][idx] = self.name[idx];
+            }
+            self.historyList[self.historyListLen][self.letterCount + 1] = 0;
+            self.historyListWordLen[self.historyListLen] = self.letterCount + 1;
+            self.historyListLen += 1;
+            self.name[0] = 0;
+            self.letterCount = 0;
         }
 
         return;
@@ -59,34 +69,28 @@ pub const Game = struct {
     fn draw(self: *Game) void {
         raylib.clearBackground(raylib.Color.white);
 
-        var thing: [50]u8 = undefined;
-        var counter: usize = 0;
-        for (self.fullLabel.items, 0..) |i, idx| {
-            if (i == 0 or i == undefined) break;
-            thing[idx] = i;
-            counter += 1;
+        if (self.historyListLen > 0) {
+            for (self.historyList, 0..) |_, i| {
+                if (i >= self.historyListLen) {
+                    break;
+                }
+                // std.log.info("historyList[{d}] = {d}", .{ i, self.historyList[i] });
+                raylib.drawText(self.historyList[i][0 .. self.historyListWordLen[i] + 0 :0], 100, @intCast(50 + (50 * i)), 20, raylib.Color.light_gray);
+            }
         }
-        thing[counter] = 0;
-
-        raylib.drawText(self.name[0..self.letterCount-1 :0], 190, 200, 20, raylib.Color.light_gray);
+        if (self.letterCount != 0) {
+            raylib.drawText(self.name[0..self.letterCount :0], 300, 200, 20, raylib.Color.light_gray);
+        }
     }
 
     pub fn destroy(self: *Game) void {
-        for (self.historyList) |i| {
-            i.deinit();
-        }
-        self.fullLabel.deinit();
-        self.currentWord.deinit();
+        _ = self;
     }
 };
 
 pub fn init(allocator: std.mem.Allocator) Game {
-    // var alloc = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = alloc.allocator();
-
     var historyList = .{string.init(allocator)} ** 5;
-    var fullLabel = string.init(allocator);
-    var currentWord = string.init(allocator);
-    var game = Game{ .historyList = historyList, .fullLabel = fullLabel, .currentWord = currentWord, .allocator = allocator };
+    _ = historyList;
+    var game = Game{ .allocator = allocator };
     return game;
 }
